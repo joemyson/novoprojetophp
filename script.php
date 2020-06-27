b203712582ae36d3995a9b2ee5584d2c8b8b9e51
joemyson-santos <joemyson1402@gmail.com>
2020-06-27 01:10:46 -0300

    começo

---
+++ script.php
@@ -0,0 +1,21 @@
+<?php
+
+$categorias = [];
+$categorias[]='infantil';
+$categorias[] ="adolescente";
+$categorias[] = "adulto";
+$categorias[] = "idoso"
+
+
+// print_r (categorias)
+
+
+
+
+
+$nome = eduardo;
+
+
+
+
+?>
---
+++ testephp.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="">
+    <title>Formulario de inscrição</title>
+</head>
+<body>
+    <p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+    <FORM action="script.php" method= "post">
+
+    <p>seu nome: <input type="text" name = "nome"/></p>
+    <p>sua idade: <input type="text" name = "idade"/></p>
+
+    <p><input type="submit"/></p>
+    
+    
+    
+    
+    </FORM>
+</body>
+</html>
