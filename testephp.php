53cc74471bf93e3b2fd0f34b4befe8feeaad9d64
joemyson-santos <joemyson1402@gmail.com>
2020-07-05 17:47:30 -0300

    corrigido

--- testephp.php
+++ testephp.php
@@ -9,6 +9,22 @@
     <p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
     <FORM action="script.php" method= "post">
+    <?php
+
+    $mensagemDeSucesso = isset($_SESSION['mensagem de sucesso'])?$_SESSION['mensagem de sucesso']
+
+    if (!empty($mensagemDeSucesso))
+    {
+        echo $mensagemDeSucesso;
+    }
+    $mensagemDeErro = isset($_SESSION['mensagem de Erro'])?$_SESSION['mensagem de Erro']
+
+    if (!empty($mensagemDeErro))
+    {
+        echo $mensagemDeErro;
+    }
+
+    ?>
 
     <p>seu nome: <input type="text" name = 'nome'/></p>
     <p>sua idade: <input type="text" name = 'idade'/></p>
